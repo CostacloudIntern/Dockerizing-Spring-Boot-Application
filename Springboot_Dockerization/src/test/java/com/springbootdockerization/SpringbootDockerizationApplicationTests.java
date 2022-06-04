e409560d2fa254e1493b66bd7206b74b36d42a45
Vibhooti <vibho@192.168.1.33>
2022-06-05 02:25:05 +0530

    Commit

---
+++ Springboot_Dockerization/src/test/java/com/springbootdockerization/SpringbootDockerizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdockerization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
